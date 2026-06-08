3af605075fb3dce4573e855f26070de3647cc7f4
cogniqarushi <cogniq.arushi@gmail.com>
2026-06-09 00:17:08 +0530

    Delete gbd-construction-redesign directory

--- gbd-construction-redesign/check.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  try {
-    const res = await fetch('https://cdn.pixabay.com/video/2021/08/25/86367-593641261_large.mp4', { method: 'HEAD' });
-    console.log("Status:", res.status);
-  } catch(e) {
-    console.error(e);
-  }
-})();
