4603e7c201f162768e489700fcfa6471d7d5ef43
cogniqarushi <cogniq.arushi@gmail.com>
2026-06-09 01:12:19 +0530

    Add files via upload

---
+++ gbd-construction-redesign (8)/test-fetch2.js
@@ -0,0 +1,1 @@
+fetch('https://ais-dev-j3g5k2o5lp6tigjqatbckb-20339417581.asia-east1.run.app/api/chat', {method: 'POST', body: JSON.stringify({history:[], message:'hi'}), headers: {'Content-Type': 'application/json'}}).then(r=> { console.log(r.status); return r.text()}).then(t=>console.log(t.substring(0,200)))
